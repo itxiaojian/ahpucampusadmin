25c3644af61ecd2a609979896e7e768f01bfff0b
itxiaojian <2629690209@qq.com>
2018-12-29 01:08:02 +0800

    消息发布调优

--- ahpucampus-rest/src/main/java/com/stylefeng/guns/rest/modular/ahpucampus/controller/MessageController.java
+++ ahpucampus-rest/src/main/java/com/stylefeng/guns/rest/modular/ahpucampus/controller/MessageController.java
@@ -1,6 +1,8 @@
 package com.stylefeng.guns.rest.modular.ahpucampus.controller;
 
 import com.alibaba.fastjson.JSONObject;
+import com.baomidou.mybatisplus.mapper.EntityWrapper;
+import com.baomidou.mybatisplus.mapper.Wrapper;
 import com.stylefeng.guns.rest.modular.ahpucampus.model.ActionResponse;
 import com.stylefeng.guns.rest.modular.ahpucampus.model.Message;
 import com.stylefeng.guns.rest.modular.ahpucampus.service.IMessageService;
@@ -12,7 +14,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Date;
+import java.util.List;
 
 /**
  * 消息发布控制器
@@ -37,6 +41,24 @@ public class MessageController {
 
         JSONObject responsedata = new JSONObject();
         responsedata.put("messageId",messageId);
+
+        return ActionResponse.success(responsedata);
+    }
+
+    @RequestMapping("/queryList")
+    @ResponseBody
+    public ActionResponse<?> queryMessageList(HttpServletRequest request) {
+        String page = request.getParameter("page");
+        String pageSize = request.getParameter("pageSize");
+        String _object = request.getParameter("_object");
+        String sign = request.getParameter("sign");
+        Object o = request.getParameterNames();
+        Wrapper<Message> messageEntityWrapper = new EntityWrapper<Message>();
+        List<Message> message = iMessageService.selectList(messageEntityWrapper);
+
+
+        JSONObject responsedata = new JSONObject();
+        responsedata.put("list",message);
 
         return ActionResponse.success(responsedata);
     }
